68f7ebf04ba9b22454ac2c03f09454a8c5707732
sharaafnazeer <ahamedsharaaf@gmail.com>
2020-06-13 11:28:10 +0530

    fix

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -95,7 +95,7 @@ class BrandController extends Controller
             if (!File::exists($path)) File::makeDirectory($path, 0755);
 
             $imageSave = Image::make($image);
-            $imageSave->resize(600, 600);
+            $imageSave->resize(160, 100);
             $imageSave->save($path . '/' . $filename);
 
             $imageUrl = url($directory . $filename);
@@ -207,7 +207,7 @@ class BrandController extends Controller
                 if (!File::exists($path)) File::makeDirectory($path, 0755);
 
                 $imageSave = Image::make($image);
-                $imageSave->resize(160, 50);
+                $imageSave->resize(160, 100);
                 $imageSave->save($path . '/' . $filename);
 
                 $imageUrl = url($directory . $filename);
